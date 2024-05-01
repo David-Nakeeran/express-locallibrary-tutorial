2a82e9e95a66619090771aa89d36c4a00a465552
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-05-01 14:33:43 +0100

    ready to be deployed

--- app.js
+++ app.js
@@ -4,14 +4,59 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+require('dotenv').config();
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const mongoDB = process.env.MONGODB_URI || process.env.DEV_MONGODB_URI;
+
+async function main() {
+  await mongoose.connect(mongoDB);
+};
+
+async function connectToMongoDB() {
+  try {
+    await main();
+  } catch(error) {
+    console.error("Error connecting to MongoDB:", error)
+  }
+};
+connectToMongoDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+// Apply rate limiter to all requests
+app.use(limiter);
+app.use(compression()); // Compress all routes
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
